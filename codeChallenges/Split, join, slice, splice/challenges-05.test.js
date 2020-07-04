a69c08183c10a84c02cbe34c5b83086fdd0ec9d3
3madov-77 <emadzxy7@gmail.com>
2020-07-05 02:01:03 +0300

    finshed 5th chalnge

--- codeChallenges/Split, join, slice, splice/challenges-05.test.js
+++ codeChallenges/Split, join, slice, splice/challenges-05.test.js
@@ -165,6 +165,10 @@ Return a new array containing just the verbs. For example, ['Mix until evenly di
 const stepActions = (recipe) => {
   let result = [];
   // Solution code here...
+  recipe.steps.forEach(item => {
+    let x = item.split(' ');
+    result.push(x[0])
+  })
   return result;
 };
 
