abbeecef9a8e6c7087a627a1c28ff810a0e7ed74
3madov-77 <emadzxy7@gmail.com>
2020-07-07 23:53:51 +0300

    finshed 5th Challenge

--- codeChallenges/Map/challenges-07.test.js
+++ codeChallenges/Map/challenges-07.test.js
@@ -79,6 +79,7 @@ For example: charCode(['h','i']) returns [104, 105].
 
 const charCode = (arr) => {
   // Solution code here...
+  return arr.map(item => item.charCodeAt(0))
 };
 
 /* ------------------------------------------------------------------------------------------------
