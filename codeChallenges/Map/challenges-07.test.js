6711d6463954fe915edafee6e0e02f3c2ae979fb
3madov-77 <emadzxy7@gmail.com>
2020-07-07 22:46:03 +0300

    finshed 2nd Challenge

--- codeChallenges/Map/challenges-07.test.js
+++ codeChallenges/Map/challenges-07.test.js
@@ -36,6 +36,7 @@ For example, twoToThe([1,2,3]) returns [2,4,8] because 2 ^ 1 = 2, 2 ^ 2 = 4, and
 
 const forLoopTwoToThe = (arr) => {
   // Solution code here...
+  return arr.map(item =>  Math.pow(2, item))
 };
 
 /* ------------------------------------------------------------------------------------------------
