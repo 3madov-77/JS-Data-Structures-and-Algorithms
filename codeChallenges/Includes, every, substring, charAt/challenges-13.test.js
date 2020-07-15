74d071fcaa5b15760530472eec128f4912c50fec
3madov-77 <emadzxy7@gmail.com>
2020-07-15 05:17:46 +0300

    finshed 1st challenge

--- codeChallenges/Includes, every, substring, charAt/challenges-13.test.js
+++ codeChallenges/Includes, every, substring, charAt/challenges-13.test.js
@@ -21,6 +21,7 @@ const $ = createSnippetWithJQuery(`
 
 const fixTheTypo = () => {
 // Solution code here...
+    $('.pear').text('Pear')
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -33,6 +34,9 @@ For example, ['this is great :)', 'wow', 'whyyyyyy :(', ':)))))'] returns ['t', 
 
 const firstLetters = (arr) => {
   // Solution code here...
+  return arr.map(item =>{
+    return item.charAt(0)
+  })
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -175,7 +179,7 @@ describe('Testing challenge 1', () => {
   });
 });
 
-describe('Testing challenge 2', () => {
+xdescribe('Testing challenge 2', () => {
   test('It should return the first letter of each element of the array', () => {
     const words = ['apple', 'banana', 'cantaloupe'];
 
@@ -185,7 +189,7 @@ describe('Testing challenge 2', () => {
   });
 });
 
-describe('Testing challenge 3', () => {
+xdescribe('Testing challenge 3', () => {
   test('It should return only the strings that contain smiley faces', () => {
     const words = ['things', 'apple (:)', ':)banana', 'missing that thing', 'cant:)aloupe'];
 
@@ -196,7 +200,7 @@ describe('Testing challenge 3', () => {
   });
 });
 
-describe('Testing challenge 4', () => {
+xdescribe('Testing challenge 4', () => {
   test('It should return a standardized set of phone numbers', () => {
     const nums = ['(123) 456-7890', '(222) 222-2222'];
 
