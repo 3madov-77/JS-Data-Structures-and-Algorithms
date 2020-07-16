f1697db816468882ee1867b10b5a4157b801499c
3madov-77 <emadzxy7@gmail.com>
2020-07-16 09:36:50 +0300

    finshed 5th challenge

--- codeChallenges/Filter/challenges-08.test.js
+++ codeChallenges/Filter/challenges-08.test.js
@@ -126,6 +126,7 @@ For example, getStatName(snorlaxData.stats, 50) will return ['special-defense', 
 
 const getStatName = (arr, minBaseStat) => {
   // Solution code here...
+
 };
 
 /* ------------------------------------------------------------------------------------------------
